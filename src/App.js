efcef762ae7f7d07fbdd33b311276d666cf9bba3
Muskaan Sharma <65418085+MuskaanSharma30@users.noreply.github.com>
2020-10-13 21:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
